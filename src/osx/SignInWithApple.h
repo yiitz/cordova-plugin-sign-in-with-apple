e301ef445241ebdbaf0128fcc58f550fde207bfc
Tamara Lytvynova <tamara.lytvynova@geeksforless.com>
2020-12-29 19:05:09 +0200

    Add OSX support

---
+++ src/osx/SignInWithApple.h
@@ -0,0 +1,14 @@
+#import <AuthenticationServices/AuthenticationServices.h>
+#import <Cordova/CDVPlugin.h> // this already includes Foundation.h
+
+#ifndef SignInWithApple_h
+#define SignInWithApple_h
+
+@interface SignInWithApple : CDVPlugin
+- (void)pluginInitialize;
+- (NSArray<ASAuthorizationScope> *)convertScopes:(NSArray<NSNumber *> *)scopes;
+- (void)signin:(CDVInvokedUrlCommand *)command;
+- (void)getCredentialState:(CDVInvokedUrlCommand *)command;
+@end
+
+#endif /* SignInWithApple_h */
